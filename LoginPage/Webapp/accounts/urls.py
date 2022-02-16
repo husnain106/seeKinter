d72d64c0bb9d65b8e9dbf218f52dd283370cc1ef
r67067rz <ruoqing.zheng@student.manchester.ac.uk>
2022-02-16 14:00:49 +0000

    add login page to the file

---
+++ LoginPage/Webapp/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+
+    path('', views.home),
+    path('products/', views.products),
+    path('customer/', views.customer),
+    
+
+]
