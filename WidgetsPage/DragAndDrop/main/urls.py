820da67e21b3fe74d1daf2ea1d173e6643f6b646
Rishi <rishi.ravikumar@student.manchester.ac.uk>
2022-02-27 15:20:17 +0000

    Created a project page

--- WidgetsPage/DragAndDrop/main/urls.py
+++ WidgetsPage/DragAndDrop/main/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path('', views.main)]
+urlpatterns = [path('', views.main), path('dd/', views.render_dd)]
--- WidgetsPage/DragAndDrop/main/views.py
+++ WidgetsPage/DragAndDrop/main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def main(request):
+    return render(request, 'main/proj2.html')
+
+def render_dd(request):
     return render(request, 'main/dd.html')
